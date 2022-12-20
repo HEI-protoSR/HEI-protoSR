07bf702f975a0972ceb8625e704dec28ac4adcb5
S Roach <roachsm6@gmail.com>
2022-12-20 15:02:17 +0000

    save6

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -64,6 +64,10 @@ var init = function (window) {
 
             // TODO 9 : Iterate over the array
            
+            for (var i = 0; i < circles.length; i++) {
+                game.checkCirclePosition(circles[i]);
+
+            }
             
         }
     
